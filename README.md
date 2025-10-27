0fb32c41e8752e10d3b03beef07d82a989824549
Astitva23 <astitvaa.k07@gmail.com>
2025-10-27 11:12:52 +0530

    azaz

--- README.md
+++
@@ -1,3 +0,0 @@
-# voice-checks
-checks out your voice quality gng
-the chat gpt chat- https://chatgpt.com/share/68fe4b10-1fa8-800f-96ee-3b6cf6153e88
--- test2.py
+++
@@ -1,9 +0,0 @@
-from speechbrain.pretrained import EncoderClassifier
-
-print("🧠 Loading SpeechBrain model (VoxLingua107)...")
-model = EncoderClassifier.from_hparams(source="speechbrain/lang-id-voxlingua107-ecapa")
-print("✅ Model loaded successfully.")
-
-print("🎧 Identifying accent...")
-result = model.classify_file("voice.wav")
-print("Accent / Language Detected:", result)
